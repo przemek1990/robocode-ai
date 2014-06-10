fffd6f632bd380e19075401532661cd24f1ed75d
Przemek <przemyslaw.pioro@schibsted.pl>
2014-06-10 18:19:08 +0200

    rule processor

--- src/rule/RulesProcessor.java
+++ src/rule/RulesProcessor.java
@@ -2,10 +2,12 @@ package rule;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.rmi.RemoteException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.rules.InvalidRuleSessionException;
 import javax.rules.RuleRuntime;
 import javax.rules.RuleServiceProvider;
 import javax.rules.RuleServiceProviderManager;
@@ -13,65 +15,31 @@ import javax.rules.StatelessRuleSession;
 import javax.rules.admin.RuleAdministrator;
 import javax.rules.admin.RuleExecutionSet;
 
+import org.jruleengine.RuleServiceProviderImpl;
+
 public class RulesProcessor {
-	private String file ="C:\\projekty\\robocode-ai\\bin\\robots\\behaviour2.xml";
-	private StatelessRuleSession session = null;
+	
+	private StatelessRuleSession statelessRuleSession = createRuleSession();
 
-	public RulesProcessor() {
-		this.init();
-	}
-
-	private void init() {
-		try {
-			session = createRuleSession();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@SuppressWarnings("rawtypes")
 	public List process(Object... inputObjects) {
-		try {
-			// Execute the rules without a filter.
-			List results = session.executeRules(Arrays.asList(inputObjects));
-			// session.release();
-			return results;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+			try {
+				return statelessRuleSession.executeRules(Arrays.asList(inputObjects));
+			} catch (InvalidRuleSessionException | RemoteException e) {
+				throw new RuntimeException(e);
+			}
 	}
 
-	@SuppressWarnings("rawtypes")
-	private StatelessRuleSession createRuleSession() throws Exception {
-		// Load the rule service provider of the reference
-		// implementation.
-		// Loading this class will automatically register this
-		// provider with the provider manager.
-		Class.forName("org.jruleengine.RuleServiceProviderImpl");
-
-		// Get the rule service provider from the provider manager.
-		RuleServiceProvider serviceProvider = RuleServiceProviderManager.getRuleServiceProvider("org.jruleengine");
-
-		// get the RuleAdministrator
-		RuleAdministrator ruleAdministrator = serviceProvider.getRuleAdministrator();
-
-		// get an input stream to a test XML ruleset
-		// This rule execution set is part of the TCK.
-		InputStream inStream = new FileInputStream(this.file);
-
-		// parse the ruleset from the XML document
-		RuleExecutionSet res1 = ruleAdministrator.getLocalRuleExecutionSetProvider(null).createRuleExecutionSet(inStream, null);
-		inStream.close();
-
-		// register the RuleExecutionSet
-		String uri = res1.getName();
-		ruleAdministrator.registerRuleExecutionSet(uri, res1, null);
-
-		RuleRuntime ruleRuntime = serviceProvider.getRuleRuntime();
-
-		// create a StatelessRuleSession
-		StatelessRuleSession statelessRuleSession = (StatelessRuleSession) ruleRuntime
-				.createRuleSession(uri, new HashMap(), RuleRuntime.STATELESS_SESSION_TYPE);
-		return statelessRuleSession;
+	private StatelessRuleSession createRuleSession()  {
+		try(InputStream fileInputStream = new FileInputStream("C:\\projekty\\robocode-ai\\bin\\robots\\behaviour2.xml");){
+			Class.forName(RuleServiceProviderImpl.class.getName());
+			RuleServiceProvider ruleServiceProvider = RuleServiceProviderManager.getRuleServiceProvider("org.jruleengine");
+			RuleAdministrator ruleAdministrator = ruleServiceProvider.getRuleAdministrator();		
+			RuleExecutionSet ruleExecutionSet = ruleAdministrator.getLocalRuleExecutionSetProvider(null).createRuleExecutionSet(fileInputStream, null);
+			ruleAdministrator.registerRuleExecutionSet(ruleExecutionSet.getName(), ruleExecutionSet, null);
+			RuleRuntime ruleRuntime = ruleServiceProvider.getRuleRuntime();
+			return (StatelessRuleSession) ruleRuntime.createRuleSession(ruleExecutionSet.getName(), new HashMap(), RuleRuntime.STATELESS_SESSION_TYPE);
+		}catch(Exception exception){
+			throw new RuntimeException(exception);
+		}
 	}
 }
